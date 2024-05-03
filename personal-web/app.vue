0ad4c2544ba1a40d7d53d0df89d02ee077592cde
Sami Mnif <samimnif@icloud.com>
2024-05-03 02:00:16 -0400

    nuxt init

---
+++ personal-web/app.vue
@@ -0,0 +1,5 @@
+<template>
+  <NuxtLayout></NuxtLayout>
+  <NuxtPage />
+</template>
+
---
+++ personal-web/layouts/default.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+const route = useRoute()
+
+const links = [{
+  label: 'Profile',
+  avatar: {
+    src: 'https://avatars.githubusercontent.com/u/66852892?v=4'
+  },
+  to: '/'
+}, {
+  label: 'Resume',
+  icon: 'i-heroicons-document-text',
+  to: `/resume`
+}, {
+  label: 'Contact Info',
+  icon: 'i-heroicons-user-circle',
+  to: '/components/command-palette'
+}, {
+  label: 'Projects',
+  icon: 'i-heroicons-rectangle-stack',
+  to: '/components/command-palette'
+}]
+</script>
+
+<template>
+  <UHorizontalNavigation :links="links" class="border-b border-gray-200 dark:border-gray-800" />
+</template>
